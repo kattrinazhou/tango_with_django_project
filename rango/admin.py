847801f65358c835b4eb076b007727aa1ce02daa
Xinuo zhou <2550838z@student.gla.ac.uk>
2022-02-02 21:48:03 +0000

    chapter9

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 
 
@@ -12,3 +13,5 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category', 'url')
 
 admin.site.register(Page, PageAdmin)
+
+admin.site.register(UserProfile)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -91,6 +91,12 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -105,9 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {  'min_length': 6, }
+    }
 ]
-
-
+LOGIN_URL = '/rango/login/'
+LOGIN_URL = 'rango:login'
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
