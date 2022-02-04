cec11c918d3fac6427ae977ae9dd7cf52e4d365c
Xinuo zhou <2550838z@student.gla.ac.uk>
2022-02-04 22:52:42 +0000

    chapter10

--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 from django.urls import reverse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
 
 def index(request):
-
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
@@ -15,15 +15,27 @@ def index(request):
     context_dict['boldmessage'] ='Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request, 'rango/index.html', context=context_dict)
+
+    visitor_cookie_handler(request)
+    request.session['visits']
+
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    return  response
 
 
 def about(request):
-    #context_dict = {'boldmessage': 'This tutorial has been put together by Xinuo Zhou.'}
     print(request.method)
     print(request.user)
-    return render(request, 'rango/about.html', {})
-    #return HttpResponse("Rango says here is the about page."+"<a href='/rango/'>Index</a>")
+
+    context_dict = {}
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/about.html', context=context_dict)
+
+    return response
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -161,3 +173,28 @@ def restriced(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                                'last_visit',
+                                                str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+
+    request.session['visits'] = visits
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -139,3 +139,6 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_COOKIE_AGE = 1209600
